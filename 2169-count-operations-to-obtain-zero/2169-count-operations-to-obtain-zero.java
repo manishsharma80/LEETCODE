3747a2a31e9ba830d9576818f87546db92104f21
manishsharma80 <manish.sharma_cs23@gla.ac.in>
2025-06-18 10:20:52 +0530

    Time: 1 ms (91.44%), Space: 40.3 MB (85.37%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+   int a =Math.max(num1,num2);
+   int b= 0;
+   if(num1==0|| num2 ==0) return b;
+   for (int i=0;i<a;i++){
+    if (num1>=num2){
+    num1-=num2;
+    }
+    else {
+        num2-=num1;}
+        b++;
+    
+   if (num1==0||num2==0) return b;
+    
+    }
+    return b;
+    }
+}
+    
